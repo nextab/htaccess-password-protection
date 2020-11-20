2b7e6a0aaee9100c2c0193864f498c3a2c23b42f
nexTab - Oliver Gehrmann <info@nextab.de>
2020-11-20 11:42:48 +0100

    Initial path.php

---
+++ path.php
@@ -0,0 +1,5 @@
+<?php
+$dir = dirname(__FILE__);
+echo "<p>Full path to this dir:<br />" . $dir . "</p>";
+echo "<p>Full path to a .htpasswd file in this dir:<br />" . $dir . "/.htpasswd" . "</p>";
+?>
